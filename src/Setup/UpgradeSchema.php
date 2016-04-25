d490eb3838a9577ead3a2d0665d3cd1cb75e495e
Kassim Belghait <kassim@sirateck.com>
2016-04-26 00:02:27 +0200

    Add Card entity
    Add link and card list in customer account

--- src/Setup/UpgradeSchema.php
+++ src/Setup/UpgradeSchema.php
@@ -99,6 +99,110 @@ class UpgradeSchema implements UpgradeSchemaInterface
         	
         	$setup->getConnection()->createTable($table);
         }
+        
+        if (version_compare($context->getVersion(), '2.0.2', '<')) {
+        	/**
+        	 * Create table 'hipay_customer_card'
+        	 * 
+        	 */
+        	$table = $setup->getConnection()
+        	->newTable($setup->getTable('hipay_customer_card'))
+        	->addColumn(
+        			'card_id',
+        			\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+        			null,
+        			['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+        			'Card Id'
+        			)
+        	->addColumn(
+        		'customer_id',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+        		null,
+        		['unsigned' => true, 'nullable' => false],
+        		'Customer Id'
+        	)
+        	->addColumn(
+        		'name',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		100,
+        		['nullable' => false],
+        		'Name of card'
+        	)
+        	->addColumn(
+        		'cc_type',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		150,
+        		['nullable' => false],
+        		'Card type'
+        	)
+        	->addColumn(
+        		'cc_exp_month',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		2,
+        		['nullable' => false],
+        		'Card expiration month'
+        	)
+        	->addColumn(
+        		'cc_exp_year',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		2,
+        		['nullable' => false],
+        		'Card expiration year'
+        	)
+        	->addColumn(
+        		'cc_secure_verify',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		10,
+        		[],
+        		'Card secure verify'
+        	)
+        	->addColumn(
+        		'cc_last4',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		4,
+        		[],
+        		'Card last 4 digits'
+        	)
+        	->addColumn(
+        		'cc_owner',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		100,
+        		[],
+        		'Card Owner'
+        	)
+        	->addColumn(
+        		'cc_number_enc',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		40,
+        		['nullable' => false],
+        		'Card Number Encrypted'
+        	)
+        	->addColumn(
+        		'cc_status',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+        		1,
+        		['nullable' => false],
+        		'Card status'
+        	)
+        	->addColumn(
+        		'cc_token',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+        		'2M',
+        		['nullable' => false],
+        		'HiPay token'
+        	)
+        	->addColumn(
+        		'is_default',
+        		\Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+        		1,
+        		['nullable' => false,'default'=>'0'],
+        		'Card by default'
+        	)
+        	;
+        
+        															 
+			$setup->getConnection()->createTable($table);
+        }
 
         $setup->endSetup();
     }
