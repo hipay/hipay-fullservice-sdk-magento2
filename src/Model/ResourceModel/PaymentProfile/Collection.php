5d9ad53ecbaefe88153dc1038313d456f9303799
Kassim Belghait <kassim@sirateck.com>
2016-05-08 14:16:13 +0200

    Add Payment profile collection class

---
+++ src/Model/ResourceModel/PaymentProfile/Collection.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * HiPay fullservice SDK
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @copyright      Copyright (c) 2016 - HiPay
+ * @license        http://opensource.org/licenses/mit-license.php MIT License
+ *
+ */
+namespace HiPay\FullserviceMagento\Model\ResourceModel\PaymentProfile;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * Initialize resource
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('HiPay\FullserviceMagento\Model\PaymentProfile', 'HiPay\FullserviceMagento\Model\ResourceModel\PaymentProfile');
+    }
+
+}
