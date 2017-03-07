482d1e4930653c7b6508127d80fc4cd0f406b07b
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-03-07 18:13:17 +0100

    Adding custom data support

--- src/Model/Request/Order.php
+++ src/Model/Request/Order.php
@@ -18,141 +18,217 @@ namespace HiPay\FullserviceMagento\Model\Request;
 use HiPay\FullserviceMagento\Model\Request\AbstractRequest as BaseRequest;
 use HiPay\Fullservice\Gateway\Request\Order\OrderRequest;
 
-/**
- * Order Request Object
- *
- * @package HiPay\FullserviceMagento
- * @author Kassim Belghait <kassim@sirateck.com>
- * @copyright Copyright (c) 2016 - HiPay
- * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 Licence
- * @link https://github.com/hipay/hipay-fullservice-sdk-magento2
- */
-class Order extends BaseRequest{
-	
-	/**
-	 * Order
-	 *
-	 * @var \Magento\Sales\Model\Order
-	 */
-	protected $_order;
-	
-	/**
-	 * Payment Method
-	 *
-	 * @var \HiPay\Fullservice\Request\AbstractRequest
-	 */
-	protected $_paymentMethod;
-	
-	protected $_ccTypes = array(
-			'VI'=>'visa',
-			'AE'=>'american-express',
-			'MC'=>'mastercard',
-			'MI'=>'maestro'
-	);
+class Order extends BaseRequest
+{
+    /**
+     * Order
+     *
+     * @var \Magento\Sales\Model\Order
+     */
+    protected $_order;
 
-	/**
-	 * {@inheritDoc}
-	 * @see \HiPay\FullserviceMagento\Model\Request\AbstractRequest::__construct()
-	 */
-	public function __construct(
-			\Psr\Log\LoggerInterface $logger,
-			\Magento\Checkout\Helper\Data $checkoutData,
-			\Magento\Customer\Model\Session $customerSession,
-			\Magento\Checkout\Model\Session $checkoutSession,
-			\Magento\Framework\Locale\ResolverInterface $localeResolver,
-			\HiPay\FullserviceMagento\Model\Request\Type\Factory $requestFactory,
-			\Magento\Framework\Url $urlBuilder,
-			\HiPay\FullserviceMagento\Helper\Data $helper,
-			$params = []
-			)
-	{
-		
-		parent::__construct($logger, $checkoutData, $customerSession, $checkoutSession, $localeResolver, $requestFactory, $urlBuilder,$helper,$params);
-		
-		
-		if (isset($params['order']) && $params['order'] instanceof \Magento\Sales\Model\Order) {
-			$this->_order = $params['order'];
-		} else {
-			throw new \Exception('Order instance is required.');
-		}
-		
-		if (isset($params['paymentMethod']) && $params['paymentMethod'] instanceof \HiPay\Fullservice\Request\AbstractRequest) {
-			$this->_paymentMethod = $params['paymentMethod'];
-		} else {
-			throw new \Exception('Object Request PaymentMethod instance is required.');
-		}
-		
-	}
-	
-	protected function getCcTypeHipay($mageCcType){
-		$hipayCcType = $mageCcType;
-		if(in_array($mageCcType,array_keys($this->_ccTypes))){
-			$hipayCcType = $this->_ccTypes[$mageCcType];
-		}
-		return $hipayCcType;
-	}
+    /**
+     * Payment Method
+     *
+     * @var \HiPay\Fullservice\Request\AbstractRequest
+     */
+    protected $_paymentMethod;
 
-	/**
-	 * Check if requested ECI is MO/TO
-	 * @return bool 
-	 */
-	protected function isMOTO(){
-		$eci = $this->_order->getForcedEci() ?: $this->_order->getPayment()->getAdditionalInformation('eci');
-		
-		if($eci == \HiPay\Fullservice\Enum\Transaction\ECI::MOTO){
-			return true;
-		}
-		
-		return false;
-	}
+    protected $_ccTypes = array(
+        'VI' => 'visa',
+        'AE' => 'american-express',
+        'MC' => 'mastercard',
+        'MI' => 'maestro'
+    );
 
-	
-	/**
-	 * @return \HiPay\Fullservice\Gateway\Request\Order\OrderRequest
-	 */
-	protected function mapRequest(){
-		/* @var $httpRequest  \Magento\Framework\App\Request\Http */
-		//$httpRequest = $this->_context->getRequest();
-		
-		$orderRequest = new OrderRequest();
-		$orderRequest->orderid = $this->_order->getForcedOrderId() ?: $this->_order->getIncrementId();
-		$orderRequest->operation = $this->_order->getForcedOperation() ?: $this->_order->getPayment()->getMethodInstance()->getConfigData('payment_action');
-		$orderRequest->payment_product = $this->getCcTypeHipay($this->_order->getPayment()->getCcType()) ?: "cb"; 
-		$orderRequest->description = $this->_order->getForcedDescription() ?: sprintf("Order #%s",$this->_order->getIncrementId()); //@TODO
-		$orderRequest->long_description = "";
-		$orderRequest->currency = $this->_order->getBaseCurrencyCode();
-		$orderRequest->amount = $this->_order->getForcedAmount() ?: (float)$this->_order->getBaseGrandTotal();
-		$orderRequest->shipping = (float)$this->_order->getShippingAmount();
-		$orderRequest->tax = (float)$this->_order->getTaxAmount();
-		$orderRequest->cid = $this->_customerId;
-		$orderRequest->ipaddr = $this->_order->getRemoteIp();
-		
-		$redirectParams = ['_secure' => true];
-		
-		if($this->isMOTO()){
-			$redirectParams['is_moto'] = true;
-		}
-		
-		$orderRequest->accept_url = $this->_urlBuilder->getUrl('hipay/redirect/accept',$redirectParams);
-		$orderRequest->pending_url = $this->_urlBuilder->getUrl('hipay/redirect/pending',$redirectParams);
-		$orderRequest->decline_url =  $this->_urlBuilder->getUrl('hipay/redirect/decline',$redirectParams);
-		$orderRequest->cancel_url =  $this->_urlBuilder->getUrl('hipay/redirect/cancel',$redirectParams); 
-		$orderRequest->exception_url =  $this->_urlBuilder->getUrl('hipay/redirect/exception',$redirectParams);
-		
-		//$orderRequest->http_accept = $httpRequest->getHeader('Accept');
-		//$orderRequest->http_user_agent = $httpRequest->getHeader('User-Agent');
-		
-		$orderRequest->device_fingerprint = "";
-		
-		$orderRequest->language = $this->_localeResolver->getLocale();
-		
-		$orderRequest->paymentMethod = $this->_paymentMethod;
-		
-		$orderRequest->customerBillingInfo = $this->_requestFactory->create('\HiPay\FullserviceMagento\Model\Request\Info\BillingInfo',['params' => ['order' => $this->_order,'config' => $this->_config]])->getRequestObject();
-		$orderRequest->customerShippingInfo = $this->_requestFactory->create('\HiPay\FullserviceMagento\Model\Request\Info\ShippingInfo',['params' => ['order' => $this->_order,'config' => $this->_config]])->getRequestObject();
-		
-		return $orderRequest;
-		
-	}
+    /**
+     * Customer Factory
+     *
+     * @var \Magento\Sales\Model\Order
+     */
+    protected $_customerRepositoryInterface;
 
+    /**
+     *
+     * @var  \Magento\Customer\Api\GroupRepositoryInterface
+     */
+    protected $_groupRepositoryInterface;
+
+
+    /**
+     * @var \HiPay\FullserviceMagento\Helper\Data
+     */
+    protected $helper;
+
+    /**
+     * {@inheritDoc}
+     * @see \HiPay\FullserviceMagento\Model\Request\AbstractRequest::__construct()
+     */
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Checkout\Helper\Data $checkoutData,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Framework\Locale\ResolverInterface $localeResolver,
+        \HiPay\FullserviceMagento\Model\Request\Type\Factory $requestFactory,
+        \Magento\Framework\Url $urlBuilder,
+        \HiPay\FullserviceMagento\Helper\Data $helper,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepositoryInterface,
+        \Magento\Customer\Api\GroupRepositoryInterface $groupRepositoryInterface,
+        $params = []
+    ) {
+        parent::__construct($logger, $checkoutData, $customerSession, $checkoutSession, $localeResolver,
+            $requestFactory, $urlBuilder, $helper, $params);
+
+        $this->helper = $helper;
+        $this->_customerRepositoryInterface = $customerRepositoryInterface;
+        $this->_groupRepositoryInterface = $groupRepositoryInterface;
+
+        if (isset($params['order']) && $params['order'] instanceof \Magento\Sales\Model\Order) {
+            $this->_order = $params['order'];
+        } else {
+            throw new \Exception('Order instance is required.');
+        }
+
+        if (isset($params['paymentMethod']) && $params['paymentMethod'] instanceof \HiPay\Fullservice\Request\AbstractRequest) {
+            $this->_paymentMethod = $params['paymentMethod'];
+        } else {
+            throw new \Exception('Object Request PaymentMethod instance is required.');
+        }
+    }
+
+
+    protected function getCcTypeHipay($mageCcType)
+    {
+        $hipayCcType = $mageCcType;
+        if (in_array($mageCcType, array_keys($this->_ccTypes))) {
+            $hipayCcType = $this->_ccTypes[$mageCcType];
+        }
+        return $hipayCcType;
+    }
+
+    /**
+     * Check if requested ECI is MO/TO
+     * @return bool
+     */
+    protected function isMOTO()
+    {
+        $eci = $this->_order->getForcedEci() ?: $this->_order->getPayment()->getAdditionalInformation('eci');
+
+        if ($eci == \HiPay\Fullservice\Enum\Transaction\ECI::MOTO) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * @return \HiPay\Fullservice\Gateway\Request\Order\OrderRequest
+     */
+    protected function mapRequest()
+    {
+        $orderRequest = new OrderRequest();
+        $orderRequest->orderid = $this->_order->getForcedOrderId() ?: $this->_order->getIncrementId();
+        $orderRequest->operation = $this->_order->getForcedOperation() ?: $this->_order->getPayment()->getMethodInstance()->getConfigData('payment_action');
+        $orderRequest->payment_product = $this->getCcTypeHipay($this->_order->getPayment()->getCcType()) ?: "cb";
+        $orderRequest->description = $this->_order->getForcedDescription() ?: sprintf("Order #%s",
+            $this->_order->getIncrementId()); //@TODO
+        $orderRequest->long_description = "";
+        $orderRequest->currency = $this->_order->getBaseCurrencyCode();
+        $orderRequest->amount = $this->_order->getForcedAmount() ?: (float)$this->_order->getBaseGrandTotal();
+        $orderRequest->shipping = (float)$this->_order->getShippingAmount();
+        $orderRequest->tax = (float)$this->_order->getTaxAmount();
+        $orderRequest->cid = $this->_customerId;
+        $orderRequest->ipaddr = $this->_order->getRemoteIp();
+
+        $redirectParams = ['_secure' => true];
+
+        if ($this->isMOTO()) {
+            $redirectParams['is_moto'] = true;
+        }
+
+        $orderRequest->accept_url = $this->_urlBuilder->getUrl('hipay/redirect/accept', $redirectParams);
+        $orderRequest->pending_url = $this->_urlBuilder->getUrl('hipay/redirect/pending', $redirectParams);
+        $orderRequest->decline_url = $this->_urlBuilder->getUrl('hipay/redirect/decline', $redirectParams);
+        $orderRequest->cancel_url = $this->_urlBuilder->getUrl('hipay/redirect/cancel', $redirectParams);
+        $orderRequest->exception_url = $this->_urlBuilder->getUrl('hipay/redirect/exception', $redirectParams);
+
+        $orderRequest->device_fingerprint = "";
+
+        $orderRequest->language = $this->_localeResolver->getLocale();
+
+        $orderRequest->paymentMethod = $this->_paymentMethod;
+
+        $orderRequest->customerBillingInfo = $this->_requestFactory->create('\HiPay\FullserviceMagento\Model\Request\Info\BillingInfo',
+            ['params' => ['order' => $this->_order, 'config' => $this->_config]])->getRequestObject();
+        $orderRequest->customerShippingInfo = $this->_requestFactory->create('\HiPay\FullserviceMagento\Model\Request\Info\ShippingInfo',
+            ['params' => ['order' => $this->_order, 'config' => $this->_config]])->getRequestObject();
+        $orderRequest->device_fingerprint = "";
+
+        $orderRequest->language = $this->_localeResolver->getLocale();
+
+        $orderRequest->paymentMethod = $this->_paymentMethod;
+
+        $orderRequest->customerBillingInfo = $this->_requestFactory->create('\HiPay\FullserviceMagento\Model\Request\Info\BillingInfo',
+            ['params' => ['order' => $this->_order, 'config' => $this->_config]])->getRequestObject();
+        $orderRequest->customerShippingInfo = $this->_requestFactory->create('\HiPay\FullserviceMagento\Model\Request\Info\ShippingInfo',
+            ['params' => ['order' => $this->_order, 'config' => $this->_config]])->getRequestObject();
+
+        // Technical parameter to track wich magento version is used
+        $orderRequest->source = $this->helper->getRequestSource();
+
+        /**
+         *  Custom Data
+         *
+         *  You can use these parameters to submit custom values you wish to show in HiPay back office transaction details,
+         *  receive back in the API response messages, in the notifications or to activate specific FPS rules.
+         *
+         *  Please make an Magento 2 plugin which listen the method "getCustomData"
+         *  of the class "HiPay\FullserviceMagento\Helper\Data"
+         */
+        $customData = $this->getCustomData();
+        if (!empty($customData)) {
+            $orderRequest->custom_data = json_encode($customData);
+        }
+
+        return $orderRequest;
+    }
+
+    /**
+     *  Generate custom data to send to HiPay back office
+     *
+     * @return array
+     */
+    public function getCustomData()
+    {
+        $customData = array();
+        return $customData;
+    }
+
+
+    /**
+     *
+     * @return \Magento\Sales\Model\Order
+     */
+    public function getOrder()
+    {
+        return $this->_order;
+    }
+
+    /**
+     * @return \Magento\Customer\Api\CustomerRepositoryInterface|\Magento\Sales\Model\Order
+     */
+    public function getCustomerRepositoryInterface()
+    {
+        return $this->_customerRepositoryInterface;
+    }
+
+    /**
+     * @return \Magento\Customer\Api\CustomerRepositoryInterface|\Magento\Sales\Model\Order
+     */
+    public function getGroupRepositoryInterface()
+    {
+        return $this->_groupRepositoryInterface;
+    }
 }
---
+++ src/Plugin/CustomDataPlugin.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * HiPay Plugin
+ *
+ * Override "getCustomData" Method in HiPay\FullserviceMagento\Helper
+ *
+ * Used to customize custom data field
+ *
+ * @package HiPay\FullserviceMagento
+ * @author Aymeric Berthelot <aberthelot@hipay.com>
+ * @copyright Copyright (c) 2017 - HiPay
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 Licence
+ * @link https://github.com/hipay/hipay-fullservice-sdk-magento2
+ */
+namespace HiPay\FullserviceMagento\Plugin;
+
+use \HiPay\FullserviceMagento\Model\Request\Order as Order;
+
+class CustomDataPlugin
+{
+
+    /**
+     *  Complete general getCustomData with HiPay'datas
+     *
+     * @see \HiPay\FullserviceMagento\Helper\Data
+     * @param \HiPay\FullserviceMagento\Model\Request\Order $subject
+     * @param $result
+     * @return array
+     */
+    public function afterGetCustomData(Order $subject, $result)
+    {
+        $order = $subject->getOrder();
+        $payment = $order->getPayment();
+
+        // Shipping description
+        $result['shipping_description'] = $order->getShippingDescription();
+
+        // Customer information
+        if ($order->getCustomerId()) {
+            $customerId = $order->getCustomerId();
+            $customer = $subject->getCustomerRepositoryInterface()->getById($customerId);
+            $group = $subject->getGroupRepositoryInterface()->getById($customer->getGroupId());
+            $result['customer_code'] = $group->getCode();
+        }
+
+        // Method payment information
+        $result['payment_code'] = $payment->getMethodInstance()->getCode();
+        $result['display_iframe'] = 0;
+
+        if ($payment->getMethodInstance()->getConfigData('iframe_mode')) {
+            $result['display_iframe'] = $payment->getMethodInstance()->getConfigData('iframe_mode');
+        }
+
+        /* Wait feature
+        if ($split_number) {
+            $customData['payment_type'] = 'Split ' . $split_number;
+        }*/
+
+        // Use OneClick
+        if ($payment->getAdditionalInformation('create_oneclick')) {
+            $result['payment_type'] = 'OneClick';
+        }
+        return $result;
+    }
+}
