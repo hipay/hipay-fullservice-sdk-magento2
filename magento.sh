274683ac89f80c35f0df9dd44398bb8c8e861b16
Landais Etienne <elandais@hipay.com>
2018-06-21 11:59:25 +0200

    Fix partial refund

--- magento.sh
+++ magento.sh
@@ -34,7 +34,7 @@ if [ "$1" = 'init' ];then
         rm -Rf data/ log/ web/
         docker-compose -f docker-compose.dev.yml build --no-cache
         docker-compose -f docker-compose.dev.yml up -d
-        docker cp magento2-hipay-fullservice:/var/www/html/magento2 web/
+        docker cp hipayfullservicesdkmagento2_magento2_1:/var/www/html/magento2 web/
         docker-compose logs -f
     else
         echo "Put your credentials in auth.env and hipay.env before start update the docker-compose.dev to link this files"
@@ -43,21 +43,21 @@ elif [ "$1" = 'restart' ];then
     docker-compose stop
     docker-compose -f docker-compose.dev.yml up -d
 elif [ "$1" = 'static' ];then
-    docker exec magento2-hipay-fullservice rm -Rf /var/www/html/magento2/pub/static/frontend/Magento/luma/en_US/HiPay_FullserviceMagento/
-    docker exec magento2-hipay-fullservice gosu magento2 php bin/magento setup:static-content:deploy -t Magento/luma
-    docker exec magento2-hipay-fullservice gosu magento2 php bin/magento c:c
+    docker exec hipayfullservicesdkmagento2_magento2_1 rm -Rf /var/www/html/magento2/pub/static/frontend/Magento/luma/en_US/HiPay_FullserviceMagento/
+    docker exec hipayfullservicesdkmagento2_magento2_1 gosu magento2 php bin/magento setup:static-content:deploy -t Magento/luma
+    docker exec hipayfullservicesdkmagento2_magento2_1 gosu magento2 php bin/magento c:c
 elif [ "$1" = 'di' ];then
-    docker exec magento2-hipay-fullservice rm -Rf /var/www/html/magento2/var/cache /var/www/html/magento2/var/di /var/www/html/magento2/var/generation /var/www/html/magento2/var/page_cache
-    docker exec magento2-hipay-fullservice gosu magento2 php bin/magento setup:di:compile
-    docker exec magento2-hipay-fullservice gosu magento2 php bin/magento c:c
+    docker exec hipayfullservicesdkmagento2_magento2_1 rm -Rf /var/www/html/magento2/var/cache /var/www/html/magento2/var/di /var/www/html/magento2/var/generation /var/www/html/magento2/var/page_cache
+    docker exec hipayfullservicesdkmagento2_magento2_1 gosu magento2 php bin/magento setup:di:compile
+    docker exec hipayfullservicesdkmagento2_magento2_1 gosu magento2 php bin/magento c:c
 elif [ "$1" = 'command' ];then
     docker exec magento2-hipay-fullservice gosu magento2 php bin/magento $2
 elif [ "$1" = 'l' ];then
     docker-compose logs -f
 elif [ "$1" = 'install' ];then
-    docker exec magento2-hipay-fullservice gosu magento2 bin/magento module:enable --clear-static-content HiPay_FullServiceMagento
-    docker exec magento2-hipay-fullservice gosu magento2 bin/magento setup:upgrade
-    docker exec magento2-hipay-fullservice gosu magento2 bin/magento c:c
+    docker exec hipayfullservicesdkmagento2_magento2_1 gosu magento2 bin/magento module:enable --clear-static-content HiPay_FullServiceMagento
+    docker exec hipayfullservicesdkmagento2_magento2_1 gosu magento2 bin/magento setup:upgrade
+    docker exec hipayfullservicesdkmagento2_magento2_1 gosu magento2 bin/magento c:c
 elif [ "$1" = 'test' ]; then
 
     rm -rf bin/tests/errors/*
