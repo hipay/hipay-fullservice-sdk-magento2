de819608d9abd0a7ab0e21a8f77edce7065bf040
Landais Etienne <elandais@hipay.com>
2018-09-04 15:38:37 +0200

    Fix Exception on failure order

--- Controller/Redirect/Decline.php
+++ Controller/Redirect/Decline.php
@@ -102,7 +102,7 @@ class Decline extends Fullservice
                     }
                 }
 
-                $cart->getResource()->save($cart);
+                $cart->save();
             }
         }
         //MO/TO case
