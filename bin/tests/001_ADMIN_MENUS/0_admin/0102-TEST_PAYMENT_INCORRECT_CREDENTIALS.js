35ab4ed989db82bc28a37047b491e33a8952c98c
Landais Etienne <elandais@hipay.com>
2018-09-12 10:01:08 +0200

    Prepare release

--- bin/tests/001_ADMIN_MENUS/0_admin/0102-TEST_PAYMENT_INCORRECT_CREDENTIALS.js
+++ bin/tests/001_ADMIN_MENUS/0_admin/0102-TEST_PAYMENT_INCORRECT_CREDENTIALS.js
@@ -43,7 +43,7 @@ casper.test.begin('Test Payment With Incorrect Credentials', function(test) {
             test.assertHttpStatus(200, "Correct HTTP Status Code 200");
             test.assertSelectorHasText(
                 '.message.message-error.error',
-                "\n        There was an error request new transaction: Incorrect Credentials : API User Not Found.\n    ",
+                "\n        There was an error request new transaction: Incorrect Credentials : API User Not Found\n.\n    ",
                 "Correct response from Magento server !"
             );
         }, function fail() {
