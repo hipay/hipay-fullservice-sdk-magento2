48eb0b8351d5d4d86a2ca0f70ab44ac48c12a576
BERTHELOT Aymeric <lemaildeaymeric@gmail.com>
2019-01-16 11:33:02 +0100

    Deploy multi version

--- bin/docker/images/generate_dockerfile.sh
+++ bin/docker/images/generate_dockerfile.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir bin/docker/images/$1
+mkdir bin/docker/images/2-3-0
 
 # Generate DockerFile
-cp -f bin/docker/images/default/Dockerfile bin/docker/images/$1/Dockerfile
-cp -f bin/docker/images/default/entrypoint.sh bin/docker/images/$1/entrypoint.sh
+cp -f bin/docker/images/default/Dockerfile bin/docker/images/2-3-0/Dockerfile
+cp -f bin/docker/images/default/entrypoint.sh bin/docker/images/2-3-0/entrypoint.sh
 
 # Change the clause FROM
-sed -i -e "s/FROM hipay\/hipay-magento2:2.1.10/FROM hipay\/hipay-magento2:$1/" bin/docker/images/$1/Dockerfile
-
-sed -i -e "s/images\/default/images\/$1/" bin/docker/images/$1/Dockerfile
+sed -i -e "s/FROM hipay\/hipay-magento2:2.1.10/FROM hipay\/hipay-magento2:2.3.0/" bin/docker/images/2-3-0/Dockerfile
 
 # Generate docker_compose
-cp -f docker-compose.test.yml docker-compose.test-$1.yml
+cp -f docker-compose.acceptance.yml docker-compose.acceptance-2-3-0.yml
 
-sed -i -e "s/images\/default/images\/$1/" docker-compose.test-$1.yml
-sed -i -e "s/\${DOCKER_STACK}-\${DOCKER_SERVICE}_web:\${CI_COMMIT_REF_SLUG}/\${DOCKER_STACK}-\${DOCKER_SERVICE}_web-$1:\${CI_COMMIT_REF_SLUG}/" docker-compose.test-$1.yml
+sed -i -e "s/images\/default/images\/2-3-0/" docker-compose.acceptance-2-3-0.yml
+sed -i -e "s/\${DOCKER_STACK}-\${DOCKER_SERVICE}_web:\${CI_COMMIT_REF_SLUG}/\${DOCKER_STACK}-\${DOCKER_SERVICE}_web-2-3-0:\${CI_COMMIT_REF_SLUG}/" docker-compose.acceptance-2-3-0.yml
