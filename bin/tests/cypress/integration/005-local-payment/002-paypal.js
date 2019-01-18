d9f9f4459409ba6ecaade69ccd44146d571dbb1f
BERTHELOT Aymeric <lemaildeaymeric@gmail.com>
2019-01-18 17:39:51 +0100

    Wait checkout rework to test paypal headless

--- bin/tests/cypress/integration/005-local-payment/002-paypal.js
+++ bin/tests/cypress/integration/005-local-payment/002-paypal.js
@@ -1,11 +1,12 @@
+
 describe('Pay by paypal', function () {
 
     /**
      * Before
      */
     before(function () {
-        //cy.configureAndActivatePaymentMethod("hipay_paypalapi");
-        //cy.get('.account-signout').click({force: true});
+        cy.configureAndActivatePaymentMethod("hipay_paypalapi");
+        cy.get('.account-signout').click({force: true});
     });
 
     /**
@@ -25,12 +26,12 @@ describe('Pay by paypal', function () {
     it('Pay with Paypal', function () {
         cy.get('#hipay_paypalapi').click();
         cy.get(".payment-method._active > .payment-method-content > .actions-toolbar:visible button").click();
-        if (Cypress.env('completeProviderPayment')) {
-            cy.payPaypal();
-            cy.checkOrderRedirect();
-        } else {
+        // if (Cypress.env('completeProviderPayment')) {
+        //     cy.payPaypal();
+        //     cy.checkOrderRedirect();
+        // } else {
             cy.location('host', {timeout: 100000}).should('include', 'sandbox.paypal');
-        }
+        // }
     });
 
 });
