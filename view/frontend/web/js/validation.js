d7e16ef840f1e8717e5859460013ff8d910b860d
Axel29 <axel.bouaziz@hotmail.fr>
2019-01-22 15:57:28 +0100

    Fix customer registration form validation
    Customer registration form validation not being triggered anymore.

--- view/frontend/web/js/validation.js
+++ view/frontend/web/js/validation.js
@@ -1,9 +1,11 @@
-require([
+define([
     'jquery',
     'mage/validation',
     'Magento_Payment/js/model/credit-card-validation/cvv-validator',
     'Magento_Payment/js/model/credit-card-validation/credit-card-data'
 ], function($,validation,cvvValidator,creditCardData){
+    'use strict';
+    
     $.validator.addMethod(
         'validate-cvv-hipay', function (value, element) {
             if (creditCardData.creditCard.type != 'MI' || value != '' ) {
@@ -11,6 +13,7 @@ require([
                 return cvvValidator(value, maxLength).isValid;
             }
             return true;
-        }, $.mage.__('Please enter a valid credit card verification number.'));
-
+        }, 
+        $.mage.__('Please enter a valid credit card verification number.')
+    );
 });
