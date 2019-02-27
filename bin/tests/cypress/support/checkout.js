40a4ab9d42478d5a27ee9f36a43c4661cc0fd7d3
Landais Etienne <elandais@hipay.com>
2019-02-27 15:54:45 +0100

    Init tests

--- bin/tests/cypress/support/checkout.js
+++ bin/tests/cypress/support/checkout.js
@@ -8,6 +8,7 @@ Cypress.Commands.add("selectItemAndGoToCart", () => {
     cy.route('/customer/section/load/**').as('getCustomerSection');
     cy.route('/review/product/listAjax/**').as('getProduct');
     cy.wait('@getProduct', {"timeout": 15000});
+    cy.wait('@getCustomerSection');
     cy.wait(15000);
     cy.get('#product-addtocart-button').click();
     cy.wait('@getCustomerSection');
