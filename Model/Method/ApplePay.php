687971f6cf35ded4052b474084169baad1dd2031
Bréchet Adam <abrechet@hipay.com>
2021-02-23 10:58:17 +0100

    Improve iDeal Hosted Fields method

--- Model/Method/ApplePay.php
+++ Model/Method/ApplePay.php
@@ -25,7 +25,7 @@ namespace HiPay\FullserviceMagento\Model\Method;
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 Licence
  * @link https://github.com/hipay/hipay-fullservice-sdk-magento2
  */
-class ApplePay extends FullserviceMethod
+class ApplePay extends LocalHostedFields
 {
 
     const HIPAY_METHOD_CODE = 'hipay_applepay';
@@ -36,20 +36,6 @@ class ApplePay extends FullserviceMethod
     protected $_code = self::HIPAY_METHOD_CODE;
 
     /**
-     * Payment Method feature
-     *
-     * @var bool
-     */
-    protected $_canUseInternal = false;
-
-    /**
-     * Payment Method feature
-     *
-     * @var bool
-     */
-    protected $_isInitializeNeeded = true;
-
-    /**
      * Is active
      *
      * @param int|null $storeId
@@ -58,71 +44,5 @@ class ApplePay extends FullserviceMethod
     public function isActive($storeId = null)
     {
         return (bool) (int) $this->getConfigData('active', $storeId) && $this->_hipayConfig->hasCredentials(false, true);
-    }
-
-    /**
-     * Instantiate state and set it to state object
-     *
-     * @param string $paymentAction
-     * @param \Magento\Framework\DataObject $stateObject
-     * @return void
-     */
-    public function initialize($paymentAction, $stateObject)
-    {
-
-        $payment = $this->getInfoInstance();
-        $order = $payment->getOrder();
-        $order->setCanSendNewEmailFlag(false);
-        $payment->setAmountAuthorized($order->getTotalDue());
-        $payment->setBaseAmountAuthorized($order->getBaseTotalDue());
-
-        $this->processAction($paymentAction, $payment);
-
-        $stateObject->setIsNotified(false);
-    }
-
-    /**
-     * Perform actions based on passed action name
-     *
-     * @param string $action
-     * @param Magento\Payment\Model\InfoInterface $payment
-     * @return void
-     */
-    protected function processAction($action, $payment)
-    {
-        $totalDue = $payment->getOrder()->getTotalDue();
-        $baseTotalDue = $payment->getOrder()->getBaseTotalDue();
-
-        switch ($action) {
-            case \HiPay\FullserviceMagento\Model\System\Config\Source\PaymentActions::PAYMENT_ACTION_AUTH:
-                $this->authorize($payment, $baseTotalDue);
-                // base amount will be set inside
-                $payment->setAmountAuthorized($totalDue);
-                break;
-            case \HiPay\FullserviceMagento\Model\System\Config\Source\PaymentActions::PAYMENT_ACTION_SALE:
-                $payment->setAmountAuthorized($totalDue);
-                $payment->setBaseAmountAuthorized($baseTotalDue);
-                $this->capture($payment, $payment->getOrder()->getBaseGrandTotal());
-                break;
-            default:
-                break;
-        }
-    }
-
-    /**
-     * Authorize payment abstract method
-     *
-     * @param \Magento\Payment\Model\InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws LocalizedException
-     * @api
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function authorize(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        parent::authorize($payment, $amount);
-        $this->place($payment);
-        return $this;
     }
 }
