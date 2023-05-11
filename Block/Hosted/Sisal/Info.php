b2f44dbac8014ea7a9689f8158897afab7fca078
Adam BRECHET <abrechet@hipay.com>
2023-05-11 15:41:04 +0200

    Fix payment reference in info block

--- Block/Hosted/Sisal/Info.php
+++ Block/Hosted/Sisal/Info.php
@@ -49,7 +49,7 @@ class Info extends \Magento\Payment\Block\Info
 
     protected function _prepareSpecificInformation($transport = null)
     {
-        $reference = $this->getInfo()->getAdditionalInformation('reference_to_pay')['reference'];
+        $reference = $this->getInfo()->getAdditionalInformation('reference_to_pay')['barCode'];
         return new \Magento\Framework\DataObject(["Payment Reference" => $reference]);
     }
 }
