79f602eb648ddc417c62d592885148eecc8cfb4c
Adam BRECHET <abrechet@hipay.com>
2023-05-30 15:45:33 +0000

    Resolve EC-2611 "Feature/"

---
+++ Model/Method/Alma4X.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * HiPay Fullservice Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Apache 2.0 Licence
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @copyright Copyright (c) 2016 - HiPay
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 Licence
+ */
+
+namespace HiPay\FullserviceMagento\Model\Method;
+
+/**
+ * Alma 4X payment method
+ *
+ * @author    Kassim Belghait <kassim@sirateck.com>
+ * @copyright Copyright (c) 2016 - HiPay
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 Licence
+ * @link      https://github.com/hipay/hipay-fullservice-sdk-magento2
+ */
+class Alma4X extends AbstractMethodAPI
+{
+    public const HIPAY_METHOD_CODE = 'hipay_alma4X';
+
+    /**
+     * @var string
+     */
+    protected static $_technicalCode = 'alma-4x';
+
+    /**
+     * @var string
+     */
+    protected $_code = self::HIPAY_METHOD_CODE;
+
+    /**
+     * Payment Method feature
+     *
+     * @var bool
+     */
+    protected $_canUseInternal = false;
+}
