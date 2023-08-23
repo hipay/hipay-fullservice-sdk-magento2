bbfd93bbf7ec6e596906e64ab11546973aef23a1
Adam BRECHET <abrechet@hipay.com>
2023-08-23 12:25:10 +0200

    Fix display cancel button

--- Model/Request/HostedPaymentPage.php
+++ Model/Request/HostedPaymentPage.php
@@ -58,7 +58,7 @@ class HostedPaymentPage extends Order
             $hppRequest->multi_use = 1;
         }
 
-        $hppRequest->display_cancel_button = $this->_config->getValue('cancel_button');
+        $hppRequest->display_cancel_button = $this->_config->getGeneraleValue('cancel_button', 'hipay_hosted_page_management');
 
         return $hppRequest;
     }
