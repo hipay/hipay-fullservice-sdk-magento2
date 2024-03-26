e8a74526d9a0acb0809ed36fbf83313db45b8437
Adam BRECHET <abrechet@hipay.com>
2024-03-26 18:09:02 +0100

    Fix cart restoration

--- Observer/RestoreBasketObserver.php
+++ Observer/RestoreBasketObserver.php
@@ -5,6 +5,7 @@ namespace HiPay\FullserviceMagento\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Checkout\Model\Session;
+use HiPay\FullserviceMagento\Model\Config;
 
 class RestoreBasketObserver implements ObserverInterface
 {
@@ -13,10 +14,19 @@ class RestoreBasketObserver implements ObserverInterface
      */
     private $checkoutSession;
 
+    /**
+     * @var Config
+     */
+    private $hipayConfig;
+
     public function __construct(
-        Session $checkoutSession
+        Session $checkoutSession,
+        Config $hipayConfig
     ) {
         $this->checkoutSession = $checkoutSession;
+        $storeId = $this->checkoutSession->getQuote()->getStore()->getStoreId();
+        $this->hipayConfig = $hipayConfig;
+        $this->hipayConfig->setStoreId($storeId);
     }
 
     public function execute(EventObserver $observer)
@@ -27,6 +37,7 @@ class RestoreBasketObserver implements ObserverInterface
         if (
             $lastRealOrder->getPayment()
             && $lastRealOrder->getPayment()->getMethodInstance()->getConfigData('restore_cart_on_back')
+            && !$this->hipayConfig->isNotificationCronActive()
             && $lastRealOrder->getData('state') === 'pending_payment'
             && $lastRealOrder->getData('status') === 'pending_payment'
         ) {
