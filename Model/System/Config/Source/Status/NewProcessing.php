17c8cd614fddbccf55ba02717f637e5e51b2c0ec
Hicham BOUMMAAIZ <hboummaaiz.ext@hipay.com>
2024-08-12 08:02:08 +0000

    [EC-3684] Cancel Pending Order Fix

---
+++ Model/System/Config/Source/Status/NewProcessing.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * HiPay Fullservice Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @copyright Copyright (c) 2016 - HiPay
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace HiPay\FullserviceMagento\Model\System\Config\Source\Status;
+
+/**
+ * Refused Order Statuses source model
+ *
+ * @author    Kassim Belghait <kassim@sirateck.com>
+ * @copyright Copyright (c) 2016 - HiPay
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 Licence
+ * @link      https://github.com/hipay/hipay-fullservice-sdk-magento2
+ */
+class NewProcessing extends \Magento\Sales\Model\Config\Source\Order\Status
+{
+    /**
+     * @var string[]
+     */
+    protected $_stateStatuses = [
+        \Magento\Sales\Model\Order::STATE_NEW,
+        \Magento\Sales\Model\Order::STATE_PROCESSING,
+        \Magento\Sales\Model\Order::STATE_PENDING_PAYMENT,
+    ];
+}
