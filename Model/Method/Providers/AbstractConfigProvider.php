528f463d7a839c477f018d66ef7e332304d97da2
Adam BRECHET <abrechet@hipay.com>
2025-07-08 13:21:38 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- Model/Method/Providers/AbstractConfigProvider.php
+++ Model/Method/Providers/AbstractConfigProvider.php
@@ -24,7 +24,7 @@ use Psr\Log\LoggerInterface;
 /**
  * Abstract config provider
  *
- * @author    Kassim Belghait <kassim@sirateck.com>
+ * @author    PI Ecommerce <pi-ecommerce@hipay.com>
  * @copyright Copyright (c) 2016 - HiPay
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 Licence
  * @link      https://github.com/hipay/hipay-fullservice-sdk-magento2
