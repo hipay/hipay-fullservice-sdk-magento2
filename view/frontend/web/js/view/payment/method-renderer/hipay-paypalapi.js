1bb88ece7c81594d112854c49e9e6ef3bde3158e
Hicham BOUMMAAIZ <hboummaaiz.ext@hipay.com>
2025-07-21 11:10:05 +0200

    improve prettier

--- view/frontend/web/js/view/payment/method-renderer/hipay-paypalapi.js
+++ view/frontend/web/js/view/payment/method-renderer/hipay-paypalapi.js
@@ -22,8 +22,8 @@ define([
   'Magento_Checkout/js/model/quote',
   'Magento_Customer/js/customer-data',
   'Magento_Checkout/js/action/get-payment-information',
-  'domReady!',
-], function(
+  'domReady!'
+], function (
   $,
   ComponentHosted,
   ko,
@@ -31,11 +31,12 @@ define([
   fullScreenLoader,
   quote,
   customerData,
-  getPaymentInformation,
+  getPaymentInformation
 ) {
   'use strict';
 
-  var isPayPalV2 = window.checkoutConfig.payment.hipay_paypalapi.isPayPalV2 ?? '';
+  var isPayPalV2 =
+    window.checkoutConfig.payment.hipay_paypalapi.isPayPalV2 ?? '';
 
   if (isPayPalV2 === 1) {
     return ComponentDefault.extend({
@@ -45,17 +46,40 @@ define([
         hipayHostedFields: null,
         redirectAfterPlaceOrder: false,
         totals: quote.totals,
-        placeOrderStatusUrl: window.checkoutConfig.payment.hiPayFullservice.placeOrderStatusUrl.hipay_paypalapi,
-        afterPlaceOrderUrl: window.checkoutConfig.payment.hiPayFullservice.afterPlaceOrderUrl.hipay_paypalapi,
-        env: window.checkoutConfig.payment.hipay_paypalapi ? window.checkoutConfig.payment.hipay_paypalapi.env : 'stage',
-        apiUsernameTokenJs: window.checkoutConfig.payment.hipay_paypalapi ? window.checkoutConfig.payment.hipay_paypalapi.apiUsernameTokenJs : '',
-        apiPasswordTokenJs: window.checkoutConfig.payment.hipay_paypalapi ? window.checkoutConfig.payment.hipay_paypalapi.apiPasswordTokenJs : '',
-        buttonLabel: window.checkoutConfig.payment.hipay_paypalapi ? window.checkoutConfig.payment.hipay_paypalapi.button_label : 'pay',
-        buttonShape: window.checkoutConfig.payment.hipay_paypalapi ? window.checkoutConfig.payment.hipay_paypalapi.button_shape : 'pill',
-        buttonColor: window.checkoutConfig.payment.hipay_paypalapi ? window.checkoutConfig.payment.hipay_paypalapi.button_color : 'gold',
-        buttonHeight: window.checkoutConfig.payment.hipay_paypalapi ? window.checkoutConfig.payment.hipay_paypalapi.button_height : '40',
-        bnpl: window.checkoutConfig.payment.hipay_paypalapi ? window.checkoutConfig.payment.hipay_paypalapi.bnpl : true,
-        locale: window.checkoutConfig.payment.hiPayFullservice.locale ? window.checkoutConfig.payment.hiPayFullservice.locale.hipay_paypalapi : 'en_us',
+        placeOrderStatusUrl:
+          window.checkoutConfig.payment.hiPayFullservice.placeOrderStatusUrl
+            .hipay_paypalapi,
+        afterPlaceOrderUrl:
+          window.checkoutConfig.payment.hiPayFullservice.afterPlaceOrderUrl
+            .hipay_paypalapi,
+        env: window.checkoutConfig.payment.hipay_paypalapi
+          ? window.checkoutConfig.payment.hipay_paypalapi.env
+          : 'stage',
+        apiUsernameTokenJs: window.checkoutConfig.payment.hipay_paypalapi
+          ? window.checkoutConfig.payment.hipay_paypalapi.apiUsernameTokenJs
+          : '',
+        apiPasswordTokenJs: window.checkoutConfig.payment.hipay_paypalapi
+          ? window.checkoutConfig.payment.hipay_paypalapi.apiPasswordTokenJs
+          : '',
+        buttonLabel: window.checkoutConfig.payment.hipay_paypalapi
+          ? window.checkoutConfig.payment.hipay_paypalapi.button_label
+          : 'pay',
+        buttonShape: window.checkoutConfig.payment.hipay_paypalapi
+          ? window.checkoutConfig.payment.hipay_paypalapi.button_shape
+          : 'pill',
+        buttonColor: window.checkoutConfig.payment.hipay_paypalapi
+          ? window.checkoutConfig.payment.hipay_paypalapi.button_color
+          : 'gold',
+        buttonHeight: window.checkoutConfig.payment.hipay_paypalapi
+          ? window.checkoutConfig.payment.hipay_paypalapi.button_height
+          : '40',
+        bnpl: window.checkoutConfig.payment.hipay_paypalapi
+          ? window.checkoutConfig.payment.hipay_paypalapi.bnpl
+          : true,
+        locale: window.checkoutConfig.payment.hiPayFullservice.locale
+          ? window.checkoutConfig.payment.hiPayFullservice.locale
+              .hipay_paypalapi
+          : 'en_us'
       },
 
       isPlaceOrderAllowed: ko.observable(false),
@@ -63,14 +87,14 @@ define([
         !(
           window.checkoutConfig.checkoutAgreements.isEnabled &&
           window.checkoutConfig.checkoutAgreements.agreements.some(
-            (input) => input.mode == '1',
+            (input) => input.mode == '1'
           )
-        ),
+        )
       ),
       allTOC: new Map(),
       cartChangeTimeout: null,
 
-      initialize: function() {
+      initialize: function () {
         var self = this;
         self._super();
 
@@ -82,17 +106,17 @@ define([
             shape: self.buttonShape,
             height: Number(self.buttonHeight),
             color: self.buttonColor,
-            label: self.buttonLabel,
+            label: self.buttonLabel
           },
           request: {
             amount: self.safeToFixed(Number(quote.totals().base_grand_total)),
             currency: quote.totals().quote_currency_code,
-            locale: this.convertToUpperCaseAfterUnderscore(self.locale),
-          },
+            locale: this.convertToUpperCaseAfterUnderscore(self.locale)
+          }
         };
 
         // Subscribe to quote totals changes
-        quote.totals.subscribe(function(totals) {
+        quote.totals.subscribe(function (totals) {
           if (self.hipayHostedFields && totals) {
             self.updatePayPalAmount(totals);
           }
@@ -107,11 +131,11 @@ define([
       /**
        * Initialize cart change listener
        */
-      initCartChangeListener: function() {
+      initCartChangeListener: function () {
         var self = this;
         var cartData = customerData.get('cart');
 
-        cartData.subscribe(function(updatedCart) {
+        cartData.subscribe(function (updatedCart) {
           if (updatedCart && updatedCart.items) {
             // Only handle cart changes if PayPal is the active payment method
             if (self.isPayPalActive()) {
@@ -124,7 +148,7 @@ define([
       /**
        * Handle cart changes with debouncing
        */
-      handleCartChange: function() {
+      handleCartChange: function () {
         var self = this;
 
         // Clear existing timeout
@@ -133,7 +157,7 @@ define([
         }
 
         // Debounce the cart change handling
-        self.cartChangeTimeout = setTimeout(function() {
+        self.cartChangeTimeout = setTimeout(function () {
           self.refreshPaymentData();
         }, 500);
       },
@@ -141,45 +165,49 @@ define([
       /**
        * Refresh payment data when cart changes
        */
-      refreshPaymentData: function() {
+      refreshPaymentData: function () {
         var self = this;
 
         // Show loading
         fullScreenLoader.startLoader();
 
         // Get fresh payment information
-        getPaymentInformation().then(function(data) {
-          if (data.totals) {
-            // Update quote totals
-            quote.setTotals(data.totals);
+        getPaymentInformation()
+          .then(function (data) {
+            if (data.totals) {
+              // Update quote totals
+              quote.setTotals(data.totals);
 
-            // Update PayPal configuration
-            self.updatePayPalConfiguration(data.totals);
+              // Update PayPal configuration
+              self.updatePayPalConfiguration(data.totals);
 
-            // Reinitialize PayPal if needed
-            if (self.hipayHostedFields) {
-              self.reinitializePayPal();
+              // Reinitialize PayPal if needed
+              if (self.hipayHostedFields) {
+                self.reinitializePayPal();
+              }
             }
-          }
-        }).always(function() {
-          fullScreenLoader.stopLoader();
-        });
+          })
+          .always(function () {
+            fullScreenLoader.stopLoader();
+          });
       },
 
       /**
        * Update PayPal configuration with new totals
        */
-      updatePayPalConfiguration: function(totals) {
+      updatePayPalConfiguration: function (totals) {
         var self = this;
 
-        self.configHipay.request.amount = self.safeToFixed(Number(totals.base_grand_total));
+        self.configHipay.request.amount = self.safeToFixed(
+          Number(totals.base_grand_total)
+        );
         self.configHipay.request.currency = totals.quote_currency_code;
       },
 
       /**
        * Update PayPal amount (for existing instances)
        */
-      updatePayPalAmount: function(totals) {
+      updatePayPalAmount: function (totals) {
         var self = this;
 
         if (self.hipayHostedFields && totals) {
@@ -187,7 +215,7 @@ define([
             // Update the amount if PayPal SDK supports it
             if (self.hipayHostedFields.updateAmount) {
               self.hipayHostedFields.updateAmount(
-                self.safeToFixed(Number(totals.base_grand_total)),
+                self.safeToFixed(Number(totals.base_grand_total))
               );
             } else {
               // Fallback: reinitialize PayPal
@@ -203,7 +231,7 @@ define([
       /**
        * Reinitialize PayPal hosted fields
        */
-      reinitializePayPal: function() {
+      reinitializePayPal: function () {
         var self = this;
 
         // Destroy existing instance
@@ -220,27 +248,27 @@ define([
         self.isPlaceOrderAllowed(false);
 
         // Reinitialize after a short delay
-        setTimeout(function() {
+        setTimeout(function () {
           self.initHostedFields();
         }, 100);
       },
 
-      initHostedFields: function() {
+      initHostedFields: function () {
         var self = this;
 
         self.hipaySdk = new HiPay({
           username: self.apiUsernameTokenJs,
           password: self.apiPasswordTokenJs,
           environment: self.env,
-          lang: self.locale.length > 2 ? self.locale.substr(0, 2) : 'en',
+          lang: self.locale.length > 2 ? self.locale.substr(0, 2) : 'en'
         });
 
         self.hipayHostedFields = self.hipaySdk.create(
           'paypal',
-          self.configHipay,
+          self.configHipay
         );
 
-        self.hipayHostedFields.on('paymentAuthorized', function(token) {
+        self.hipayHostedFields.on('paymentAuthorized', function (token) {
           self.paymentAuthorized(self, token);
         });
 
@@ -249,13 +277,13 @@ define([
         return true;
       },
 
-      initTOCEvents: function() {
+      initTOCEvents: function () {
         var self = this;
 
-        $(document).ready(function() {
+        $(document).ready(function () {
           if (window.checkoutConfig.checkoutAgreements.isEnabled) {
-            var observer = new MutationObserver(function(mutations) {
-              mutations.forEach(function(mutation) {
+            var observer = new MutationObserver(function (mutations) {
+              mutations.forEach(function (mutation) {
                 if (
                   mutation.type === 'childList' &&
                   mutation.addedNodes.length
@@ -269,15 +297,15 @@ define([
 
           function initPaypalEvents() {
             var results = document.querySelectorAll(
-              'input[id*=\'agreement_hipay_paypal\']',
+              "input[id*='agreement_hipay_paypal']"
             );
             var agreements =
               window.checkoutConfig.checkoutAgreements.agreements;
             agreements = agreements.filter((input) => input.mode == '1');
             if (results?.length == agreements.length) {
-              results.forEach(function(input, index) {
+              results.forEach(function (input, index) {
                 self.allTOC.set(index, false);
-                input.addEventListener('change', function(event) {
+                input.addEventListener('change', function (event) {
                   self.allTOC.set(index, event.target.checked);
                   updateTOCState();
                 });
@@ -288,7 +316,7 @@ define([
 
           function updateTOCState() {
             var noChecked = [...self.allTOC.values()].filter(
-              (value) => value == false,
+              (value) => value == false
             );
             if (noChecked.length > 0) {
               self.isAllTOCChecked(false);
@@ -299,87 +327,90 @@ define([
         });
       },
 
-      paymentAuthorized: function(self, tokenHipay) {
+      paymentAuthorized: function (self, tokenHipay) {
         self.browser_info(JSON.stringify(tokenHipay.browser_info));
         self.paypal_order_id(tokenHipay.orderID);
         self.placeOrder(self.getData(), self.redirectAfterPlaceOrder);
       },
 
-      isPaypalV2Allowed: function() {
+      isPaypalV2Allowed: function () {
         return isPayPalV2 === 1;
       },
 
       /**
        * Check if PayPal is the active payment method
        */
-      isPayPalActive: function() {
+      isPayPalActive: function () {
         var self = this;
         var selectedPaymentMethod = quote.paymentMethod();
 
         // Check if PayPal is selected as payment method
-        return selectedPaymentMethod && selectedPaymentMethod.method === self.getCode();
+        return (
+          selectedPaymentMethod &&
+          selectedPaymentMethod.method === self.getCode()
+        );
       },
 
       /**
        * @param {Function} handler
        */
-      setPlaceOrderHandler: function(handler) {
+      setPlaceOrderHandler: function (handler) {
         this.placeOrderHandler = handler;
       },
 
       /**
        * @param {Function} handler
        */
-      setValidateHandler: function(handler) {
+      setValidateHandler: function (handler) {
         this.validateHandler = handler;
       },
 
-      initObservable: function() {
+      initObservable: function () {
         var self = this;
         self._super().observe(['paypal_order_id', 'browser_info']);
         return self;
       },
 
-      afterPlaceOrder: function() {
+      afterPlaceOrder: function () {
         $.mage.redirect(this.afterPlaceOrderUrl);
       },
 
-      context: function() {
+      context: function () {
         return this;
       },
 
-      getProductCode: function() {
+      getProductCode: function () {
         return 'paypal';
       },
 
-      getCode: function() {
+      getCode: function () {
         return 'hipay_paypalapi';
       },
 
-      getData: function() {
+      getData: function () {
         var self = this;
         var parent = self._super();
         var data = {
           method: self.item.method,
           additional_data: {
             paypal_order_id: self.paypal_order_id(),
-            browser_info: self.browser_info(),
-          },
+            browser_info: self.browser_info()
+          }
         };
         return $.extend(true, parent, data);
       },
 
-      isActive: function() {
+      isActive: function () {
         return true;
       },
 
-      convertToUpperCaseAfterUnderscore: function(str) {
+      convertToUpperCaseAfterUnderscore: function (str) {
         let parts = str.split('_');
         parts[1] = parts[1].toUpperCase();
         return parts.join('_');
       },
 
-      safeToFixed: function(value, decimals = 2) {
+      safeToFixed: function (value, decimals = 2) {
         const factor = 10 ** decimals;
         const correction = 1 / 10 ** (decimals + 2);
         const rounded = Math.round((value + correction) * factor) / factor;
@@ -389,12 +420,12 @@ define([
       /**
        * Cleanup method to clear timeouts
        */
-      dispose: function() {
+      dispose: function () {
         if (this.cartChangeTimeout) {
           clearTimeout(this.cartChangeTimeout);
         }
         this._super();
-      },
+      }
     });
   } else {
     return ComponentHosted.extend({
@@ -403,13 +434,13 @@ define([
         redirectAfterPlaceOrder: false
       },
 
-      getCode: function() {
+      getCode: function () {
         return 'hipay_paypalapi';
       },
 
-      isActive: function() {
+      isActive: function () {
         return true;
-      },
+      }
     });
   }
 });
