5f2fa983b3f248271be3843c6341281259c6aed3
Amara Ibrahim <iamara.ext@hipay.com>
2025-08-08 16:39:52 +0100

    PI-3975 - [MAGENTO] Utiliser le SDK JS pour Oney & Ajouter le domaine oney.io aux CSP

---
+++ Model/Method/Providers/OneyConfigProvider.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * HiPay Fullservice Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Apache 2.0 Licence
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @copyright Copyright (c) 2016 - HiPay
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 Licence
+ */
+namespace HiPay\FullserviceMagento\Model\Method\Providers;
+
+use HiPay\FullserviceMagento\Helper\Data;
+use HiPay\FullserviceMagento\Model\Config;
+use HiPay\FullserviceMagento\Model\Method\Context;
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Customer\Model\Session;
+use Magento\Framework\Locale\ResolverInterface;
+use Magento\Framework\Url;
+use Magento\Payment\Model\CcConfig;
+use Psr\Log\LoggerInterface;
+
+class OneyConfigProvider extends AbstractConfigProvider implements ConfigProviderInterface
+{
+    /**
+     * @var CcConfig
+     */
+    protected $ccConfig;
+
+    /**
+     * @var array
+     */
+    protected $methods = [];
+
+    /**
+     * Url Builder
+     *
+     * @var Url
+     */
+    protected $urlBuilder;
+
+    /**
+     * @var Data
+     */
+    protected $hipayHelper;
+
+    /**
+     * @var Session
+     */
+    protected $customerSession;
+
+    /**
+     * @var Config
+     */
+    protected $hipayConfig;
+
+    /**
+     * @var ResolverInterface
+     */
+    protected $resolver;
+
+    /**
+     * OneyConfigProvider constructor.
+     *
+     * @param CcConfig          $ccConfig
+     * @param Data              $hipayHelper
+     * @param Session           $customerSession
+     * @param Context           $context
+     * @param LoggerInterface   $logger
+     * @param ResolverInterface $resolver
+     * @param Config            $hipayConfig
+     * @param array             $methodCodes
+     */
+    public function __construct(
+        CcConfig $ccConfig,
+        Data $hipayHelper,
+        Session $customerSession,
+        Context $context,
+        LoggerInterface $logger,
+        ResolverInterface $resolver,
+        Config $hipayConfig,
+        array $methodCodes = []
+    ) {
+        parent::__construct(
+            $context,
+            $logger
+        );
+
+        $this->methods = $methodCodes;
+        $this->urlBuilder = $context->getUrlBuilder();
+        $this->hipayHelper = $hipayHelper;
+        $this->resolver = $resolver;
+        $this->customerSession = $customerSession;
+
+        $storeId = $this->resolveValidStoreId();
+
+        $this->hipayConfig = $hipayConfig;
+        $this->hipayConfig->setStoreId($storeId);
+        $this->hipayConfig->setMethodCode('');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfig()
+    {
+        $config = [];
+        foreach ($this->methods as $methodCode) {
+            $this->hipayConfig->setMethodCode($methodCode);
+            if ($this->hipayConfig->isPaymentMethodActive()) {
+                $config = array_merge_recursive(
+                    $config,
+                    [
+                        'payment' => [
+                            $methodCode => [
+                                'apiUsernameTokenJs' => $this->hipayConfig->getApiUsernameTokenJs(),
+                                'apiPasswordTokenJs' => $this->hipayConfig->getApiPasswordTokenJs(),
+                                'env' => $this->hipayConfig->getApiEnv(),
+                                'sdkJsUrl' => $this->hipayConfig->getSdkJsUrl(),
+                                'paymentProductFees' => $this->hipayConfig->getValue('payment_product_fees')
+                            ],
+                        ],
+                    ]
+                );
+            }
+        }
+
+        return $config;
+    }
+}
--- view/frontend/web/js/view/payment/method-renderer/hipay-facilypay4X.js
+++ view/frontend/web/js/view/payment/method-renderer/hipay-facilypay4X.js
@@ -14,21 +14,168 @@
  * @link      https://github.com/hipay/hipay-fullservice-sdk-magento2
  */
 define([
-  'jquery',
-  'HiPay_FullserviceMagento/js/view/payment/method-renderer/hipay-hosted'
-], function ($, Component) {
-  'use strict';
-  return Component.extend({
-    defaults: {
-      template: 'HiPay_FullserviceMagento/payment/hipay-hosted',
-      redirectAfterPlaceOrder: false
-    },
+    'jquery',
+    'ko',
+    'Magento_Checkout/js/view/payment/default',
+    'Magento_Checkout/js/model/full-screen-loader',
+    'Magento_Checkout/js/model/quote',
+    'domReady!'
+], function ($, ko, Component, fullScreenLoader, quote) {
+    'use strict';
+    return Component.extend({
+        defaults: {
+            template: 'HiPay_FullserviceMagento/payment/hipay-oney',
+            configHipay: null,
+            hipayHostedFields: null,
+            redirectAfterPlaceOrder: false,
+            afterPlaceOrderUrl: window.checkoutConfig.payment.hiPayFullservice.afterPlaceOrderUrl.hipay_facilypay4X,
+            env: window.checkoutConfig.payment.hipay_facilypay4X
+                ? window.checkoutConfig.payment.hipay_facilypay4X.env
+                : 'stage',
+            apiUsernameTokenJs: window.checkoutConfig.payment.hipay_facilypay4X
+                ? window.checkoutConfig.payment.hipay_facilypay4X.apiUsernameTokenJs
+                : '',
+            apiPasswordTokenJs: window.checkoutConfig.payment.hipay_facilypay4X
+                ? window.checkoutConfig.payment.hipay_facilypay4X.apiPasswordTokenJs
+                : '',
+            paymentProductFees: window.checkoutConfig.payment.hipay_facilypay4X
+                ? window.checkoutConfig.payment.hipay_facilypay4X.paymentProductFees
+                : '',
+            locale: window.checkoutConfig.payment.hiPayFullservice.locale
+                ? window.checkoutConfig.payment.hiPayFullservice.locale.hipay_facilypay4X
+                : 'fr_FR'
+        },
+        isPlaceOrderAllowed: ko.observable(false),
 
-    getCode: function () {
-      return 'hipay_facilypay4X';
-    },
-    isActive: function () {
-      return true;
-    }
-  });
+        initialize: function () {
+            var self = this;
+            self._super();
+
+            self.configHipay = {
+                selector: `hipay-container-oney-${self.getProductCode()}`,
+                template: 'auto',
+                request: {
+                    amount: self.safeToFixed(Number(quote.totals().base_grand_total)),
+                    currency: quote.totals().quote_currency_code
+                }
+            };
+
+            // Update amount on cart total changes
+            quote.totals.subscribe(function (totals) {
+                if (self.hipayHostedFields) {
+                    self.configHipay.request.amount = self.safeToFixed(Number(totals.base_grand_total));
+                    self.configHipay.request.currency = totals.quote_currency_code;
+                }
+            });
+
+            return self;
+        },
+
+        isRadioButtonVisible: function () {
+            return true;
+        },
+
+        isPlaceOrderActionAllowed: function () {
+            return this.isPlaceOrderAllowed();
+        },
+
+        initHostedFields: function () {
+            var self = this;
+            self.hipaySdk = new HiPay({
+                username: self.apiUsernameTokenJs,
+                password: self.apiPasswordTokenJs,
+                environment: self.env,
+                lang: self.locale.length > 2 ? self.locale.substr(0, 2) : 'fr'
+            });
+
+            self.hipayHostedFields = self.hipaySdk.create(self.paymentProductFees, self.configHipay);
+
+            self.hipayHostedFields.on('paymentAuthorized', function (response) {
+                self.paymentAuthorized(self, response);
+            });
+
+            self.isPlaceOrderAllowed(true);
+            return true;
+        },
+
+        paymentAuthorized: function (self, response) {
+            self.payment_product(response.payment_product);
+            self.browser_info(JSON.stringify(response.browser_info));
+            self.oney_order_id(response.orderID);
+            self.placeOrder(self.getData(), self.redirectAfterPlaceOrder);
+        },
+
+        setPlaceOrderHandler: function (handler) {
+            this.placeOrderHandler = handler;
+        },
+
+        setValidateHandler: function (handler) {
+            this.validateHandler = handler;
+        },
+
+        initObservable: function () {
+            var self = this;
+            self._super().observe(['payment_product', 'browser_info', 'oney_order_id']);
+            return self;
+        },
+
+        place_order: function (data, event) {
+            var self = this;
+            if (event) {
+                event.preventDefault();
+            }
+
+            fullScreenLoader.startLoader();
+            self.hipayHostedFields.getPaymentData().then(
+                function (response) {
+                    self.payment_product(response.payment_product);
+                    self.browser_info(JSON.stringify(response.browser_info));
+                    self.oney_order_id(response.orderID);
+                    self.placeOrder(self.getData(), self.redirectAfterPlaceOrder);
+                    fullScreenLoader.stopLoader();
+                },
+                function (errors) {
+                    fullScreenLoader.stopLoader();
+                }
+            );
+        },
+
+        afterPlaceOrder: function () {
+            $.mage.redirect(this.afterPlaceOrderUrl);
+        },
+
+        context: function () {
+            return this;
+        },
+
+        getProductCode: function () {
+            return '4xcb';
+        },
+
+        getCode: function () {
+            return 'hipay_facilypay4X';
+        },
+
+        getData: function () {
+            var self = this;
+            var parent = self._super();
+            var data = {
+                method: self.item.method,
+                additional_data: {
+                    payment_product: self.payment_product(),
+                    browser_info: self.browser_info(),
+                    oney_order_id: self.oney_order_id()
+                }
+            };
+            return $.extend(true, parent, data);
+        },
+
+        isActive: function () {
+            return true;
+        },
+
+        safeToFixed: function (value) {
+            return Number(value).toFixed(2);
+        }
+    });
 });
